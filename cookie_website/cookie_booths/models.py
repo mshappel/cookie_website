51495438d401e18947229b8e7628962df308153a
wataylor88 <wataylor@ncsu.edu>
2021-10-24 17:34:24 -0400

    Fleshing out cookie_booths app (#4)
    - Updated Booths to Booth_Location
    - Added (most) needed fields to Booth_Location, Booth_Day, Booth_Block
    - Added basic functionality to Booth_Block to enable/disable, reserve blocks
    - Added functionality to Booth_Day to handle block creation based on open/close times
    - Added functionality to Booth_Location to create Booth_Days as needed
    - Added unit tests
    
    Testing:
    - Unit tests passing.

--- cookie_website/cookie_booths/models.py
+++ cookie_website/cookie_booths/models.py
@@ -1,36 +1,228 @@
 from django.db import models
+from django.db.models import Q
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from django.utils import timezone
 
+MIN_BOOTH_BLOCK_HOURS = 2
 
-class Booths(models.Model):
+class Booth_Location(models.Model):
     """Contains data relevant for booths"""
-    # TODO Add Hours of Operation
+    # ID is referenced via Django object ID
     booth_location = models.CharField(max_length=300)
-    booth_requires_masks = models.BooleanField()
-    booth_is_outside = models.BooleanField()
-    booth_free_text = models.CharField(max_length=100)
-    booth_enabled = models.BooleanField()
+    booth_address = models.CharField(max_length=300)
+
+    booth_enabled = models.BooleanField(default=False)
+
+    booth_is_golden_ticket = models.BooleanField(default=False)
+    booth_requires_masks = models.BooleanField(default=False)
+    booth_is_outside = models.BooleanField(default=False)
+
+    booth_notes = models.CharField(max_length=100)
 
     class Meta:
-        verbose_name_plural = "booths"
-        verbose_name = "booth"
+        verbose_name_plural = "booth locations"
+        verbose_name = "booth location"
 
     def __str__(self):
         return self.booth_location
 
+    def add_or_update_day(self, date, open_time, close_time):
+        # First see if we have a Booth_Day existing for that date. If so, grab it and update the open/close time
+        booth_day = None
+        try:
+            booth_day = Booth_Day.objects.get(booth_day_date=date)
+        except Booth_Day.DoesNotExist:
+            # If it doesn't exist yet, create it
+            booth_day = Booth_Day.objects.create(booth=self, \
+                                                 booth_day_date=date, \
+                                                 booth_day_enabled=False, \
+                                                 booth_day_hours_set=False)
+
+        # Set the hours
+        booth_day.add_or_update_hours(open_time, close_time)
+        booth_day.save()
+
+        return
+
 
 class Booth_Day(models.Model):
     """Contains data relevant for a day of a booth"""
-    booth = models.ForeignKey(Booths, on_delete=models.CASCADE)
-    booth_day_enabled = models.BooleanField()
-    booth_day_date = models.DateField()
+    booth = models.ForeignKey(Booth_Location, on_delete=models.CASCADE)
+
+    booth_day_date = models.DateField(blank=True, null=True)
+
+    booth_day_hours_set = models.BooleanField(default=False)
+    booth_day_open_time = models.DateTimeField(blank=True, null=True)
+    booth_day_close_time = models.DateTimeField(blank=True, null=True)
+
+    booth_day_enabled = models.BooleanField(default=False)
+
+    def enable_day(self):
+        # If we're already enabled, nothing to do
+        if booth_day_enabled:
+            return
+
+        booth_day_enabled = true
+
+        for block in Booth_Block.objects.filter(booth_day__id=self.id):
+            block.booth_block_enabled = true
+
+    def disable_day(self):
+        # If we're already disabled, nothing to do
+        if not booth_day_enabled:
+            return
+
+        booth_day_enabled = False
+
+        for block in Booth_Block.objects.filter(booth_day__id=self.id):
+            block.booth_block_enabled = false
+
+
+    def add_or_update_hours(self, open_time, close_time):
+        # TODO: Enforce permission
+        # There are two main cases we need to handle here:
+        # If hours have been previously set, or if they haven't
+
+        # Case 1 - Hours have been set
+        if self.booth_day_hours_set is True:
+            # Easy escape clause - if we've already set hours,
+            # And they match what is here, then we have nothing to change
+            if open_time is self.booth_day_open_time and \
+                close_time is self.booth_day_close_time:
+                return
+
+            # So the hours differ, we need to handle this from both ends
+
+            # A few easy operations - Blocks that start before the new open time
+            # or end after the new close time should be cleared
+            Booth_Block.objects.filter(Q(booth_day__id=self.id),
+                                       Q(booth_block_start_time__lt=open_time) | Q(booth_block_end_time__gt=close_time)).delete()
+
+            if not Booth_Block.objects.filter(booth_day__id=self.id):
+                self.booth_day_hours_set = False
+            else:
+                # We should now have a pruned list based on our new open/close times.
+                # Now we need to do the following:
+                # 1. If the new open time is before our previous one, see if we need to add new blocks on the front end
+                first_block = Booth_Block.objects.filter(booth_day__id=self.id).earliest('booth_block_start_time')
+
+                start_end = (first_block.booth_block_start_time.replace(hour=first_block.booth_block_start_time.hour - 2), first_block.booth_block_start_time, True)
+                while start_end[0].hour >= open_time.hour and start_end[2]:
+                    start_end = self.__add_block_backwards(start_end)
+
+                # 2. If the new close time is after our previous one, see if we need to add new blocks on the back end
+                last_block = Booth_Block.objects.filter(booth_day__id=self.id).latest('booth_block_end_time')
+
+                start_end = (last_block.booth_block_end_time, last_block.booth_block_end_time.replace(hour=last_block.booth_block_end_time.hour + 2), True)
+                while start_end[1].hour <= close_time.hour and start_end[2]:
+                    start_end = self.__add_block_forwards(start_end)
+
+        # If, after our pruning, we end up with an empty list, we can just generate a new one based on time
+        if not self.booth_day_hours_set:
+            start_end = (open_time, open_time.replace(hour=open_time.hour+2), True)
+
+            while start_end[1].hour <= close_time.hour and start_end[2]:
+                start_end = self.__add_block_forwards(start_end)
+
+        self.booth_day_hours_set = True
+        self.booth_day_open_time = open_time
+        self.booth_day_close_time = close_time
+
+        return
+
+    # Add block forward in time
+    def __add_block_forwards(self, start_end):
+        # Create a new block
+        booth_block = Booth_Block.objects.create(booth_day=self, \
+                                                 booth_block_start_time=start_end[0], \
+                                                 booth_block_end_time=start_end[1], \
+                                                 booth_block_reserved=False, \
+                                                 booth_block_enabled=self.booth_day_enabled)
+
+        cont = True
+        # Shift hours forward, if we can
+        new_start_time = start_end[0].hour + 2
+        if new_start_time > 23:
+            new_start_time = 23
+            cont = False
+
+        new_end_time = start_end[1].hour + 2
+        if new_end_time > 23:
+            new_end_time = 23
+            cont = False
+
+        # If we hit the end of the day and there isn't enough time to add a block, we can bail on block creation
+        if new_end_time - new_start_time != 2:
+            cont = False
+
+        return start_end[0].replace(hour=new_start_time), start_end[1].replace(hour=new_end_time), cont
+
+    # Add block backward in time
+    def __add_block_backwards(self, start_end):
+        # Create a new block
+        booth_block = Booth_Block.objects.create(booth_day=self, \
+                                                 booth_block_start_time=start_end[0], \
+                                                 booth_block_end_time=start_end[1], \
+                                                 booth_block_reserved=False, \
+                                                 booth_block_enabled=self.booth_day_enabled)
+
+        cont = True
+        # Shift hours backward
+        new_start_time = start_end[0].hour - 2
+        if new_start_time < 0:
+            new_start_time = 0
+            cont = False
+
+        new_end_time = start_end[1].hour - 2
+        if new_end_time < 0:
+            new_end_time = 0
+            cont = False
+
+        # If we hit the beginning of the day and there isn't enough time to add a block, we can bail on block creation
+        if new_end_time - new_start_time != 2:
+            cont = False
+
+        return start_end[0].replace(hour=new_start_time), start_end[1].replace(hour=new_end_time), cont
 
 
 class Booth_Block(models.Model):
     """Contains information for a particular booth block"""
-    booth_day = models.ForeignKey(Booths, on_delete=models.CASCADE)
-    booth_block_reserved = models.BooleanField()
-    # TODO Determine if CharField is correct, we may need to change this to something else for the user accounts
-    # booth_block_current_troop_owner = models.CharField(max_length=10)
-    # Need time period
-    booth_block_is_golden_ticket = models.BooleanField()
-    booth_block_enabled = models.BooleanField()
+    booth_day = models.ForeignKey(Booth_Day, on_delete=models.CASCADE)
+
+    booth_block_start_time = models.DateTimeField(blank=True, null=True)
+    booth_block_end_time = models.DateTimeField(blank=True, null=True)
+
+    booth_block_current_troop_owner = models.SmallIntegerField(default=0)
+    booth_block_reserved = models.BooleanField(default=False)
+
+    booth_block_enabled = models.BooleanField(default=False)
+
+    def cancel_block(self, troop_id):
+        # TODO: Need to Enforce permissions to allow canceling only if the calling user either
+        # 1. Is associated with the troop currently reserving this block, in which case they can do this
+        # 2. Has a superuser permission and can cancel any block here
+        return
+
+    def reserve_block(self, troop_id):
+        # If this block is not enabled, no reservation can be made
+        if not self.booth_block_enabled:
+            return False
+
+        # If this block is already reserved, we cannot reserve it.
+        if self.booth_block_reserved:
+            return False
+
+        # At this point we're good to go reserving it.
+        self.booth_block_reserved = True
+        self.booth_block_current_troop_owner = troop_id
+
+        # TODO: Send email confirmation?
+
+        return True
+
+
+@receiver(pre_delete, sender=Booth_Block)
+def notify_block_deletion(sender, instance, **kwargs):
+    # TODO: email owner
+    return
--- cookie_website/cookie_booths/tests.py
+++ cookie_website/cookie_booths/tests.py
@@ -1,3 +1,204 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from cookie_booths.models import Booth_Location, Booth_Day, Booth_Block
+
+import datetime
+import pytz
+
+class BoothBlockTestCase(TestCase):
+    def test_reserve_block(self):
+        location = Booth_Location.objects.create()
+        day = Booth_Day.objects.create(booth=location)
+        block = Booth_Block.objects.create(booth_day=day, \
+                                           booth_block_enabled=False)
+
+        troop_id_1 = 5
+        troop_id_2 = 6
+
+        # TODO: Add confirmation?
+
+        # Case 1 - block is disabled, trying to reserve
+        block.reserve_block(troop_id_1)
+        self.assertTrue(block.booth_block_current_troop_owner != troop_id_1)
+        self.assertTrue(block.booth_block_reserved == False)
+
+        # Case 2 - block is enabled, allowed to reserve
+        block.booth_block_enabled = True
+        block.reserve_block(troop_id_1)
+        self.assertTrue(block.booth_block_current_troop_owner == troop_id_1)
+        self.assertTrue(block.booth_block_reserved == True)
+
+        # Case 3 - block is enabled, already reserved. A new troop cannot reserve it
+        block.reserve_block(troop_id_2)
+        self.assertTrue(block.booth_block_current_troop_owner == troop_id_1)
+        self.assertTrue(block.booth_block_reserved == True)
+
+class BoothDayTestCase(TestCase):
+    def test_add_or_update_hours(self):
+        # Setup
+        location = Booth_Location.objects.create()
+        date = datetime.date(2021, 10, 22)
+
+        day = Booth_Day.objects.create(booth=location, \
+                                       booth_day_date=date, \
+                                       booth_day_hours_set=False, \
+
+                                       booth_day_enabled=False)
+
+        open_time = datetime.datetime(2021, 10, 22, 8, 0, 0, 0, pytz.UTC)
+        close_time = datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC)
+
+        # Pre-conditions - we have no Booth_Blocks that have been created
+        self.assertTrue(Booth_Block.objects.count() == 0)
+
+        # Case 1 - we have no hours set.
+        # We're setting a 4 hour block, so in 2 hour increments, this should result in two blocks created
+        day.add_or_update_hours(open_time, close_time)
+        self.assertEqual(Booth_Block.objects.count(), 2)
+        # One should be 8-10, the other 10-12
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 8, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 10, 0, 0, 0, pytz.UTC)))
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 10, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC)))
+
+        # Case 2 - hours already set. We're moving to hours totally exclusive of the current hours
+        # All blocks currently there should be deleted, and we should have two new blocks created
+        open_time = datetime.datetime(2021, 10, 22, 14, 0, 0, 0, pytz.UTC)
+        close_time = datetime.datetime(2021, 10, 22, 18, 0, 0, 0, pytz.UTC)
+
+        day.add_or_update_hours(open_time, close_time)
+        # Confirm we still have two blocks present
+        self.assertEqual(Booth_Block.objects.count(), 2)
+        # And confirm that the time ranges are what we expected - 14-16, 16-18
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 14, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 16, 0, 0, 0, pytz.UTC)))
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 16, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 18, 0, 0, 0, pytz.UTC)))
+
+        # Case 3 - hours already set. We're going to extend the closing time. We expect a new block added on the end
+        close_time = datetime.datetime(2021, 10, 22, 20, 0, 0, 0, pytz.UTC)
+
+        day.add_or_update_hours(open_time, close_time)
+        # Confirm we now have three blocks
+        self.assertEqual(Booth_Block.objects.count(), 3)
+        # The existing blocks should not have been modified, and we'll have one new one
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 14, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 16, 0, 0, 0, pytz.UTC)))
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 16, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 18, 0, 0, 0, pytz.UTC)))
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 18, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 20, 0, 0, 0, pytz.UTC)))
+
+        # Case 4 - hours already set. Extend the closing by a not clean amount (3 hours)
+        close_time = datetime.datetime(2021, 10, 22, 23, 0, 0, 0, pytz.UTC)
+
+        day.add_or_update_hours(open_time, close_time)
+        # We should now have four blocks, with some extra dangling time at the end
+        self.assertEqual(Booth_Block.objects.count(), 4)
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 20, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 22, 0, 0, 0, pytz.UTC)))
+
+        # Case 5 - hours already set. Move the closing hours in. We expect that block we added on the end to get deleted
+        close_time = datetime.datetime(2021, 10, 22, 21, 0, 0, 0, pytz.UTC)
+
+        day.add_or_update_hours(open_time, close_time)
+        # Confirm that block was removed
+        self.assertEqual(Booth_Block.objects.count(), 3)
+        self.assertFalse(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 20, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 22, 0, 0, 0, pytz.UTC)))
+
+        # Case 6 - hours already set. Make the opening time earlier. We expect a block added on that side
+        open_time = datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC)
+
+        day.add_or_update_hours(open_time, close_time)
+        # Confirm a block was added
+        self.assertEqual(Booth_Block.objects.count(), 4)
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 14, 0, 0, 0, pytz.UTC)))
+
+        # Case 7 - hours already set. Make the opening time earlier, in an odd increment. We expect one block to be added
+        open_time = datetime.datetime(2021, 10, 22, 9, 0, 0, 0, pytz.UTC)
+
+        day.add_or_update_hours(open_time, close_time)
+        # Confirm a block was added
+        self.assertEqual(Booth_Block.objects.count(), 5)
+        self.assertTrue(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 10, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC)))
+
+        # Case 8 - hours already set. Make the opening time later. We expect a block to be deleted to move in
+        open_time = datetime.datetime(2021, 10, 22, 11, 0, 0, 0, pytz.UTC)
+
+        day.add_or_update_hours(open_time, close_time)
+        # Confirm a block was removed
+        self.assertEqual(Booth_Block.objects.count(), 4)
+        self.assertFalse(
+            Booth_Block.objects.filter(booth_block_start_time=datetime.datetime(2021, 10, 22, 10, 0, 0, 0, pytz.UTC), \
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC)))
+
+class BoothLocationTestCase(TestCase):
+    def test_add_or_update_day(self):
+        # The block creation was tested up in BoothDayTestCase.
+        # But this is just testing that this all cascades down properly from Booth_Location - creating a day,
+        # which in turn should create a number of blocks
+        location = Booth_Location.objects.create(booth_location="Walmart", \
+                                                 booth_address="123 Feels Pretty Good Kay St", \
+                                                 booth_enabled=False, \
+                                                 booth_is_golden_ticket=False, \
+                                                 booth_requires_masks=True, \
+                                                 booth_is_outside=True,
+                                                 booth_notes="You can sell cookies here")
+
+        date = datetime.datetime(2021, 10, 22)
+        date_open_time = datetime.datetime(2021, 10, 22, 8, 0, 0, 0, pytz.UTC)
+        date_close_time = datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC)
+
+        # Case 1 - Try adding a date with hours
+        location.add_or_update_day(date, date_open_time, date_close_time)
+
+        # Confirm what was constructed as a result.
+        # One Booth_Day, with the open and close times we have specified
+        self.assertEqual(Booth_Day.objects.count(), 1)
+        self.assertTrue(
+            Booth_Day.objects.filter(booth_day_date=date, \
+                                     booth_day_hours_set=True, \
+                                     booth_day_open_time=date_open_time, \
+                                     booth_day_close_time=date_close_time))
+
+        # Two Booth_Blocks, based on the open/close times
+        self.assertEqual(Booth_Block.objects.count(), 2)
+
+        # Case 2 - Edit the exiting date with updated open/close times
+        # We want to confirm the existing day was updated and a new one was not created
+        date_open_time = datetime.datetime(2021, 10, 22, 6, 0, 0, 0, pytz.UTC)
+        location.add_or_update_day(date, date_open_time, date_close_time)
+
+        # Confirm the updates - one Booth_Day still, with the parameters we expect. 3 Booth_Blocks
+        self.assertEqual(Booth_Day.objects.count(), 1)
+        self.assertTrue(
+            Booth_Day.objects.filter(booth_day_date=date, \
+                                     booth_day_hours_set=True, \
+                                     booth_day_open_time=date_open_time, \
+                                     booth_day_close_time=date_close_time))
+        self.assertEqual(Booth_Block.objects.count(), 3)
+
+        # Case 3 - add a new date. Confirm it was added separately
+        date = datetime.datetime(2021, 10, 23)
+        location.add_or_update_day(date, date_open_time, date_close_time)
+
+        # Confirm we now have two days, and six blocks (3 for each day)
+        self.assertEqual(Booth_Day.objects.count(), 2)
+        self.assertEqual(Booth_Block.objects.count(), 6)
+
+
