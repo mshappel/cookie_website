c4aa12446b9cf1b6ee42f505458d84d7d02c40d7
Alan Taylor <wataylor@ncsu.edu>
2021-10-24 14:33:39 -0400

    Fleshing out cookie_booths app
    - Updated Booths to Booth_Location
    - Added (most) needed fields to Booth_Location, Booth_Day, Booth_Block
    - Added basic functionality to Booth_Block to enable/disable, reserve blocks
    - Added functionality to Booth_Day to handle block creation based on open/close times
    - Added functionality to Booth_Location to create Booth_Days as needed
    - Added unit tests
    
    Testing:
    - Unit tests passing.

--- cookie_website/cookie_booths/admin.py
+++ cookie_website/cookie_booths/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Booths, Booth_Day, Booth_Block
+from .models import Booth_Location, Booth_Day, Booth_Block
 
-admin.site.register(Booths)
+admin.site.register(Booth_Location)
 admin.site.register(Booth_Day)
 admin.site.register(Booth_Block)
--- cookie_website/cookie_website/settings.py
+++ cookie_website/cookie_website/settings.py
@@ -115,6 +115,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+TIME_ZONE = "US/Central"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
