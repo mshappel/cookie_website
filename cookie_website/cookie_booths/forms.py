c2faaebea87e33b799e06d30bb44936db1ba214f
Alan Taylor <wataylor@ncsu.edu>
2021-11-02 22:44:48 -0400

    Set up Views Skeleton
    Add skeleton for cookie_booths/views.py
    Add some basic forms onto forms.py

--- cookie_website/cookie_booths/forms.py
+++ cookie_website/cookie_booths/forms.py
@@ -1,2 +1,61 @@
+from django import forms
+from django.utils.translation import gettext as _
+
+from .models import Booth_Location
+
 # Add forms for booths here
 
+class BoothLocationForm(forms.ModelForm):
+    class Meta:
+        model = Booth_Location
+        fields = ['booth_location',
+                  'booth_address',
+                  'booth_notes',
+                  'booth_is_golden_ticket',
+                  'booth_requires_masks',
+                  'booth_is_outside',
+                  'booth_enabled']
+
+        labels = {'booth_location': _('Location Name'),
+                  'booth_address': _('Booth Address'),
+                  'booth_notes': _('Additional Booth Notes'),
+                  'booth_is_golden_ticket': _('Golden Ticket Booth'),
+                  'booth_requires_masks': _('Booth Requires Masks'),
+                  'booth_is_outside': _('Booth Is Outside'),
+                  'booth_enabled': _('Booth Is Enabled')}
+
+        help_texts = {'booth_enabled': _('Enabled means booth blocks are able to be reserved.')}
+
+
+class BoothHoursForm(forms.Form):
+    booth_start_date = forms.DateField(help_text="Enter the date sales will start at this location (inclusive)")
+    booth_end_date = forms.DateField(help_text="Enter the date sales will end at this location (inclusive)")
+
+    # TODO: Add cleaning to enable/disable TimeFields based on BooleanField
+    sunday_open = forms.BooleanField(label="Open Sundays")
+    sunday_open_time = forms.TimeField(label="Sunday Open Time")
+    sunday_close_time = forms.TimeField(label="Sunday Close Time")
+
+    monday_open = forms.BooleanField(label="Open Monday")
+    monday_open_time = forms.TimeField(label="Monday Open Time")
+    monday_close_time = forms.TimeField(label="Monday Close Time")
+
+    tuesday_open = forms.BooleanField(label="Open Tuesday")
+    tuesday_open_time = forms.TimeField(label="Tuesday Open Time")
+    tuesday_close_time = forms.TimeField(label="Tuesday Close Time")
+
+    wednesday_open = forms.BooleanField(label="Open Wednesday")
+    wednesday_open_time = forms.TimeField(label="Wednesday Open Time")
+    wednesday_close_time = forms.TimeField(label="Wednesday Close Time")
+
+    thursday_open = forms.BooleanField(label="Open Thursday")
+    thursday_open_time = forms.TimeField(label="Thursday Open Time")
+    thursday_close_time = forms.TimeField(label="Thursday Close Time")
+
+    friday_open = forms.BooleanField(label="Open Friday")
+    friday_open_time = forms.TimeField(label="Friday Open Time")
+    friday_close_time = forms.TimeField(label="Friday Close Time")
+
+    saturday_open = forms.BooleanField(label="Open Saturday")
+    saturday_open_time = forms.TimeField(label="Saturday Open Time")
+    saturday_close_time = forms.TimeField(label="Saturday Close Time")
--- cookie_website/cookie_booths/views.py
+++ cookie_website/cookie_booths/views.py
@@ -4,3 +4,67 @@ from django.shortcuts import render
 def index(request):
     """The home page for Cookie Booths"""
     return render(request, 'cookie_booths/index.html')
+
+
+def new_location(request):
+    # Create new BoothLocationForm
+    return
+
+
+def edit_location(request, booth_id):
+    # Get existing Booth_ID, populate BoothLocationForm
+    return
+
+
+def edit_location_hours(request, booth_id):
+    # Get existing hours if set, open form
+    # Read form output, call model function to add/edit existing booth days
+    return
+
+
+def enable_location(request, booth_id, date):
+    # Enable all dates for a particular boot up to and including a particular date
+    return
+
+
+def enable_all_locations(request, date):
+    # Enable all dates for all locations up to and including a particular date
+    return
+
+
+def get_available_dates(request):
+    # Go through all booth locations, and based on an intersection of those, figure out the list of available
+    # Dates for selecting blocks
+    return
+
+
+def get_available_blocks(request, date):
+    # Go through all booth locations open on a particular date, and return all of the blocks,
+    # Probably ordered and grouped by location in some kind of (location/location_name, [block list]) tuple format
+    return
+
+
+def is_block_enabled(request, block_id):
+    # Check if block is enabled for reservation. May still be cancelable
+    return
+
+
+def is_block_reserved(request, block_id):
+    # Check if block is reserved by a user that is *not* the current user
+    return
+
+
+def is_block_reserved_by_user(request, block_id):
+    # Check if block is reserved by the current user, and thus can be cancelled
+    return
+
+
+def reserve_block(request, block_id):
+    # Attempt to reserve a block, based on the amount of tickets available to the user, FFA status, etc
+    # Some kind of error if it fails?
+    return
+
+
+def cancel_block(request, block_id):
+    # Attempt to cancel a block, considering it belongs to the current user
+    return
