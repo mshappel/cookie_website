8d7e02eac9cbdd1dbce11bea45240b653e10b42d
mshappel <36205722+mshappel@users.noreply.github.com>
2021-11-20 14:49:08 -0600

    - Added jQuery to booths.html - Updated urls + views to accommodate this - Testing: Manual

--- cookie_website/cookie_booths/urls.py
+++ cookie_website/cookie_booths/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('booths/', views.booth_locations, name='booth_locations'),
     path('new_booth_location/', views.new_location, name='new_location'),
+
+    path('load_location/', views.load_location, name='load_location')
 ]
