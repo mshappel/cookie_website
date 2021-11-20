2f2e915e8af8c35abb7d9531f230ac94198336da
wataylor88 <wataylor@ncsu.edu>
2021-11-20 17:49:22 -0500

    Add handling for calculating tickets per week (#10)
    * Add handling for calculating tickets per week
    
    - Added logic to Troop to count booths available per week
    - Fix in models/BoothBlock to save block allocation properly
    - Added tests for Troops
    
    Testing:
    Unit tests passing.
    
    * Updated constants
    
    Updated constants to caps.
    
    Testing:
    Unit tests passing.

--- cookie_website/users/models.py
+++ cookie_website/users/models.py
@@ -1,7 +1,17 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User, Group
 from django.db import models
-from django.contrib.auth.models import User, Group
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+
+from cookie_booths.models import BoothDay, BoothBlock
+
+
+class TroopTicketParameters:
+    NORMAL_TROOP_TOTAL_TICKETS_PER_WEEK = 5
+    NORMAL_TROOP_GOLDEN_TICKETS_PER_WEEK = 1
+
+    SUPER_TROOP_TOTAL_TICKETS_PER_WEEK = 10
+    SUPER_TROOP_GOLDEN_TICKETS_PER_WEEK = 2
 
 
 # User-related Models
@@ -15,24 +25,73 @@ class User(AbstractUser):
 
 
 class Troop(models.Model):
-    troop_cookie_coordinator = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
     troop_number = models.PositiveSmallIntegerField(unique=True)
+
+    troop_cookie_coordinator = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+
     super_troop = models.BooleanField(default=False)
     daisy_troop = models.BooleanField(default=False)
 
-    booth_tickets_per_week = models.PositiveSmallIntegerField(default=0)
+    total_booth_tickets_per_week = models.PositiveSmallIntegerField(default=0)
     booth_golden_tickets_per_week = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
         return 'Troop ' + str(self.troop_number)
 
+    def get_num_tickets_remaining(self, start_date, end_date):
+        # A couple of preconditions:
+        # start_date should be BEFORE end_date
+        if start_date > end_date:
+            return (0, 0)
 
+        # start_date should be a Monday, end_date should be a Sunday
+        if start_date.weekday() != 0 and end_date.weekday() != 6:
+            return (0, 0)
+
+        # They should be 6 days apart, so representing a week period inclusive
+        delta = end_date - start_date
+        if delta.days != 6:
+            return (0, 0)
+
+        # OK, we're good to check vs actual data now
+        total_booth_count = 0
+        super_booth_count = 0
+        # We're filtering by Blocks that are owned by this troop, and are associated with a BoothDay which falls into
+        # the range of [start_date, end_date] inclusive
+        for block in BoothBlock.objects.filter(booth_block_reserved=True,
+                                               booth_block_current_troop_owner=self.troop_number,
+                                               booth_day__booth_day_date__gte=start_date,
+                                               booth_day__booth_day_date__lte=end_date):
+            if block.booth_day.booth.booth_is_golden_ticket:
+                super_booth_count += 1
+
+            total_booth_count += 1
+
+        rem = 0 if (total_booth_count > self.total_booth_tickets_per_week) else (self.total_booth_tickets_per_week - total_booth_count)
+        rem_super = 0 if (super_booth_count > self.booth_golden_tickets_per_week) else (self.booth_golden_tickets_per_week - super_booth_count)
+
+        return (rem, rem_super)
+
+
+@receiver(post_save, sender=User)
 def default_group(sender, instance, created, **kwargs):
     if created:
         instance.groups.add(Group.objects.get(name='Troop Cookie Coordinator'))
 
 
-post_save.connect(default_group, sender=User)
+@receiver(pre_save, sender=Troop)
+def update_tickets(sender, instance, **kwargs):
+    # This really should only care about setting/updating these after creation, but there is always the chance
+    # A troop could be updated after the fact. In that case, if they've booked more blocks than they should've,
+    # oh well.
+    if instance.super_troop:
+        instance.total_booth_tickets_per_week = TroopTicketParameters.SUPER_TROOP_TOTAL_TICKETS_PER_WEEK
+        instance.booth_golden_tickets_per_week = TroopTicketParameters.SUPER_TROOP_GOLDEN_TICKETS_PER_WEEK
+    else:
+        instance.total_booth_tickets_per_week = TroopTicketParameters.NORMAL_TROOP_TOTAL_TICKETS_PER_WEEK
+        instance.booth_golden_tickets_per_week = TroopTicketParameters.NORMAL_TROOP_GOLDEN_TICKETS_PER_WEEK
+
+
 # Groups
 # Administrators
 # Service Unit Cookie Coordinator
--- cookie_website/users/tests.py
+++ cookie_website/users/tests.py
@@ -1,3 +1,96 @@
+from datetime import date, datetime
 from django.test import TestCase
 
+from .models import Troop, TroopTicketParameters
+from cookie_booths.models import BoothLocation, BoothDay, BoothBlock
+
+import datetime
+import pytz
+
 # Create your tests here.
+class TroopTestCase(TestCase):
+    def test_ticket_allocation(self):
+        normal_troop = Troop.objects.create(troop_number=3)
+        super_troop = Troop.objects.create(troop_number=4,
+                                           super_troop=True)
+
+        # Validate the ticket allocation of both
+        self.assertTrue(normal_troop.total_booth_tickets_per_week ==
+                        TroopTicketParameters.NORMAL_TROOP_TOTAL_TICKETS_PER_WEEK)
+        self.assertTrue(normal_troop.booth_golden_tickets_per_week ==
+                        TroopTicketParameters.NORMAL_TROOP_GOLDEN_TICKETS_PER_WEEK)
+
+        self.assertTrue(super_troop.total_booth_tickets_per_week ==
+                        TroopTicketParameters.SUPER_TROOP_TOTAL_TICKETS_PER_WEEK)
+        self.assertTrue(super_troop.booth_golden_tickets_per_week ==
+                        TroopTicketParameters.SUPER_TROOP_GOLDEN_TICKETS_PER_WEEK)
+
+        # Now create a booth day to test around - First a golden ticket
+        golden_location = BoothLocation.objects.create(booth_is_golden_ticket=True)
+        date = datetime.date(2021, 10, 22)
+
+        golden_day = BoothDay.objects.create(booth=golden_location,
+                                             booth_day_date=date,
+                                             booth_day_hours_set=False,
+                                             booth_day_enabled=True)
+
+        # Going to allocate 5 blocks on this day
+        open_time = datetime.datetime(2021, 10, 22, 4, 0, 0, 0, pytz.UTC)
+        close_time = datetime.datetime(2021, 10, 22, 14, 0, 0, 0, pytz.UTC)
+        golden_day.add_or_update_hours(open_time, close_time)
+
+        # Now add our normal troop to one block (its golden ticket max), two for the super troop
+        block = BoothBlock.objects.get(booth_day=golden_day,
+                                       booth_block_start_time=datetime.datetime(2021, 10, 22, 8, 0, 0, 0, pytz.UTC),
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 10, 0, 0, 0, pytz.UTC))
+        block.reserve_block(normal_troop.troop_number)
+
+        block = BoothBlock.objects.get(booth_day=golden_day,
+                                       booth_block_start_time=datetime.datetime(2021, 10, 22, 10, 0, 0, 0, pytz.UTC),
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC))
+        block.reserve_block(super_troop.troop_number)
+
+        block = BoothBlock.objects.get(booth_day=golden_day,
+                                       booth_block_start_time=datetime.datetime(2021, 10, 22, 12, 0, 0, 0, pytz.UTC),
+                                       booth_block_end_time=datetime.datetime(2021, 10, 22, 14, 0, 0, 0, pytz.UTC))
+        block.reserve_block(super_troop.troop_number)
+
+        # Now confirm the adds have been reflected in our counts that week
+        self.assertTrue(
+            normal_troop.get_num_tickets_remaining(datetime.date(2021, 10, 18), datetime.date(2021, 10, 24)) == (4,0))
+        self.assertTrue(
+            super_troop.get_num_tickets_remaining(datetime.date(2021, 10, 18), datetime.date(2021, 10, 24)) == (8,0))
+
+        # Make sure the counts are still ok for a different week
+        self.assertTrue(
+            normal_troop.get_num_tickets_remaining(datetime.date(2021, 10, 25), datetime.date(2021, 10, 31)) == (5,1))
+        self.assertTrue(
+            super_troop.get_num_tickets_remaining(datetime.date(2021, 10, 25), datetime.date(2021, 10, 31)) == (10,2))
+
+        # Now let's create a day sometime in that next week, at a normal location, same hours
+        location = BoothLocation.objects.create()
+        date = datetime.date(2021, 10, 28)
+
+        normal_day = BoothDay.objects.create(booth=location,
+                                             booth_day_date=date,
+                                             booth_day_hours_set=False,
+                                             booth_day_enabled=True)
+
+        normal_day.add_or_update_hours(open_time, close_time)
+
+        # Add all of these blocks to our normal troop
+        for block in BoothBlock.objects.filter(booth_day=normal_day):
+            block.reserve_block(normal_troop.troop_number)
+
+        # Double check that the counts for the original week have not changed
+        self.assertTrue(
+            normal_troop.get_num_tickets_remaining(datetime.date(2021, 10, 18), datetime.date(2021, 10, 24)) == (4, 0))
+        self.assertTrue(
+            super_troop.get_num_tickets_remaining(datetime.date(2021, 10, 18), datetime.date(2021, 10, 24)) == (8, 0))
+
+        # And then make sure the counts for the new week have
+        self.assertTrue(
+            normal_troop.get_num_tickets_remaining(datetime.date(2021, 10, 25), datetime.date(2021, 10, 31)) == (0, 1))
+        self.assertTrue(
+            super_troop.get_num_tickets_remaining(datetime.date(2021, 10, 25), datetime.date(2021, 10, 31)) == (10, 2))
+
