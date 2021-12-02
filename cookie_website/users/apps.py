65c600cd423f54c48dd30e3b0125733a9ef4f82b
wataylor88 <wataylor@ncsu.edu>
2021-12-02 13:37:25 -0500

    Added groups on migration, default user permissions (#20)
    - Added Groups, default user permissions
    - Manual testing showed these working fine.
    
    Testing: Unit tests passing. Manual testing looking ok.

--- cookie_website/users/apps.py
+++ cookie_website/users/apps.py
@@ -1,6 +1,38 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+        post_migrate.connect(create_groups, sender=self)
+
+
+def create_groups(sender, **kwargs):
+    from django.contrib.auth.models import Group
+    from django.contrib.auth.models import Permission
+    from django.contrib.contenttypes.models import ContentType
+
+    from cookie_booths.models import BoothBlock
+
+    Group.objects.get_or_create(name='Troop Cookie Coordinator')
+    # TODO: Add TCC permissions?
+
+    # Normal users can reserve and cancel booth blocks. That's it.
+    group, created = Group.objects.get_or_create(name='Users')
+    content_type = ContentType.objects.get_for_model(BoothBlock)
+    cancel_perm = Permission.objects.get(
+        codename='cancel_block',
+        content_type=content_type
+    )
+    reserve_perm = Permission.objects.get(
+        codename='reserve_block',
+        content_type=content_type
+    )
+
+    group.permissions.add(cancel_perm)
+    group.permissions.add(reserve_perm)
+
+
