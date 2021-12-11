32c0dd6cd890155f903ae4c246417270fa01626d
mshappel <36205722+mshappel@users.noreply.github.com>
2021-12-11 11:24:54 -0600

    Added files needs to for go-live

--- cookie_website/cookie_website/settings.py
+++ cookie_website/cookie_website/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +136,6 @@ AUTH_USER_MODEL = 'users.User'
 
 # Cookie Settings
 LOGIN_URL = 'users:login'
+
+# Heroku settings
+django_heroku.settings(locals())
