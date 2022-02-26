897df9df5303884df1825b1d5f701f0361f3678f
mshappel <36205722+mshappel@users.noreply.github.com>
2022-02-26 15:48:17 -0600

    Made minor changes to yml and settings to hopefully get the CI working

--- cookie_website/settings.py
+++ cookie_website/settings.py
@@ -87,6 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'dbtest.sqlite3'),
+        },
     }
 }
 
