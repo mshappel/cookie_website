a3df171c66c64d741085663765195da0edf18cd7
mshappel <36205722+mshappel@users.noreply.github.com>
2022-02-25 22:48:32 -0600

    Minor update where a weird merge issue caused the views to lose an import

--- troops/views.py
+++ troops/views.py
@@ -1,7 +1,7 @@
-
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .forms import TroopForm
 from .models import Troop
