6f76e3cb4a1ef45fc73b3f02ab35a79c3c53d46d
mshappel <36205722+mshappel@users.noreply.github.com>
2022-12-11 17:46:03 -0600

    Adding forgot password, change password, and some visual fixes (#101)

--- accounts/admin.py
+++ accounts/admin.py
@@ -11,19 +11,21 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = [
         "email",
+        "first_name",
+        "last_name"
     ]
     list_filter = ["groups"]
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password', "first_name", "last_name")}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', "first_name", "last_name", 'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
-    search_fields = ('email',)
+    search_fields = ('email','first_name', 'last_name')
     ordering = ('email',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/forms.py
+++ accounts/forms.py
@@ -8,6 +8,15 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ("email", "first_name", "last_name")
 
+    def __init__(self, *args, **kwargs) -> None:
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].help_text = "Required"
+        self.fields["first_name"].required = True
+        self.fields['first_name'].help_text = "Required"
+        self.fields["last_name"].required = True
+        self.fields["last_name"].help_text = "Required"
+
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
